7ea89cf8f6f285985f1418b53cf96834116f34f8
Stefano Cirillo <ste.cirillo94@gmail.com>
2017-07-07 12:53:50 +0200

    Initial commit

---
+++ iHelp/iHelp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iHelp
+//
+//  Created by Cirillo Stefano on 07/07/17.
+//  Copyright © 2017 Cirillo Stefano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

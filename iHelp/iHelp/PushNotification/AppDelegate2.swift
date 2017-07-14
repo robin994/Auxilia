f053c1fb7037f7420104b539a39095ecba76e62f
Stefano Cirillo <ste.cirillo94@gmail.com>
2017-07-14 10:53:04 +0200

    Aggiunta gestione push notification. Installati i Pods. Integrati i metodi all'interno della classe AppDelegate Aggiunto il metodo di richiesta post per l'invio della notifica in SOS.swift

---
+++ iHelp/iHelp/PushNotification/AppDelegate2.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate2.swift
+//  PushNotification
+//
+//  Created by Cirillo Stefano on 11/07/17.
+//  Copyright © 2017 Cirillo Stefano. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseMessaging
+import UserNotifications
+
+class AppDelegate2: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate{
+    
+
+}

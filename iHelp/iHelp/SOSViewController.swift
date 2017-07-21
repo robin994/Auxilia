038de7e76369284c27e4beca30170d6b0531dba7
Lucio Grimaldi <lucio.stig@gmail.com>
2017-07-21 11:14:40 +0200

    Now in SOSViewController after notification sent Button turn green

--- iHelp/iHelp/SOSViewController.swift
+++ iHelp/iHelp/SOSViewController.swift
@@ -164,6 +164,7 @@ class SOSViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlaye
 				
 				DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 4){
 					self.infoLabel.text = "Notification Sent."
+					self.cancelButton.backgroundColor = UIColor.green
 				}
 				
 				
